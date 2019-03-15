d4cf082e5abd71830c83ad7e2024538875efd679
remat <tamer.abdelmaaboud@citynews.it>
2019-03-15 11:04:40 +0100

    Fixed v2015 component and sections

--- fractal.js
+++ fractal.js
@@ -13,7 +13,7 @@ const fractal = module.exports = require('@frctl/fractal').create();
 /*
  * Give your project a title.
  */
-fractal.set('project.title', 'Test Fractal');
+fractal.set('project.title', 'Playground Styleguide');
 
 /*
  * Tell Fractal where to look for components.
